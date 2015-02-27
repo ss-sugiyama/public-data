ae6f9a82fdca4c01ec90db4b1f72a8e471e9e222
ss-sugiyama <kashin.oct@gmail.com>
2015-02-27 20:41:50 +0900

    ファイルアップロード

---
+++ sql-lib/index.php
@@ -0,0 +1,65 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<?php /* ページのタイトル */ ?>
+<title>タイトル</title>
+
+<?php /* ファビコン登録(PCのWebタブ) */ ?>
+<link rel="shortcut icon" href="">
+
+<?php /* iPhone、iPadのWebクリップ画像(アイコン) */ ?>
+<link rel="apple-touch-icon-precomposed" href="" />
+
+<?php /* 外部フォント登録 */ ?>
+<link href='//fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Oswald:400,700' rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css' />
+<link rel='stylesheet' id='open-sans-css' href='//fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&#038;subset=latin%2Clatin-ext&#038;ver=4.0' type='text/css' media='all' />
+
+<?php /* ページの概要 */ ?>
+<meta name="description" content="">
+
+<?php /* ページのURL */ ?>
+<link rel="canonical" href="">
+
+<?php /* CSSの指定 */ ?>
+<link rel='stylesheet' href='common/style.css' type='text/css' media='all' />
+
+<?php /* CSSの直書き*/ ?>
+<style type="text/css" media="screen">
+</style>
+<?php /* JSの指定 */ ?>
+<script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js?ver=4.0'></script>
+<script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js?ver=4.0'></script>
+<script type='text/javascript' src=''></script>
+</head>
+<body>
+<div id="gloval-header">
+<header>
+<h1>テストページ</h1>
+<nav>
+<ul>
+	<li><a href="">プログラム</a></li>
+</ul>
+<ul class="header-menu">
+	<li><a href="">HTML</a></li>
+	<li><a href="">CSS</a></li>
+	<li><a href="">JavaScript</a></li>
+	<li><a href="">PHP</a></li>
+	<li><a href="">MySQL</a></li>
+	<li><a href="">サーバ</a></li>
+</ul>
+</nav>
+</header>
+<section>
+</section>
+<footer>
+</footer>
+
+<?php /* JSの直書き*/ ?>
+<script type=”text/javascript”>
+</script>
+</body>
+</html>
